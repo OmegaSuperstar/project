cad147cd1033b70e79401b69678430d3c02a7d6d
Omega <115628398+OmegaSuperstar@users.noreply.github.com>
2024-11-27 12:24:26 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ import './home.css';
 function Home(){
     return(
         <div className='HomeContainer'>
-            <h1>Welcome to the</h1>
+            <h2>Welcome to the</h2>
             <a href="https://www.leagueoflegends.com/en-us/" target="_blank"> {/* Opens the league of legends site in a new tab when clicked*/}
                 <img id="LolLogoImg" src={lolLogo} alt="League of Legends Logo"></img>
             </a>
